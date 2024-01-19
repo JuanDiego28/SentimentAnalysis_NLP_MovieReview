cd9afb9773f566bf16c85a60c94382db585722d1
JuanDiego28 <juandiego654321@gmail.com>
2024-01-19 14:56:43 -0500

    adding contraction function

--- src/text_normalizer.py
+++ src/text_normalizer.py
@@ -229,7 +229,7 @@ def expand_contractions(text, contraction_mapping=CONTRACTION_MAP) -> str:
         "({})".format("|".join(contraction_mapping.keys())),
         flags=re.IGNORECASE | re.DOTALL,
     )
-
+ 
     def expand_match(contraction):
         match = contraction.group(0)
         first_char = match[0]
